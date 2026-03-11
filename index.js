e9e0010b0dddfaff68500f4e7906dab8ff466695
AprilO5 <aprillindie@gmail.com>
2026-03-11 16:20:45 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,23 @@
+function createLoginTracker(userInfo) {
+  let attemptCount = 0;
 
+  return (loginAttempt) => {
+    attemptCount++;
 
+    if (attemptCount > 3) {
+      return "Account locked";
+    }
 
+    if (
+      loginAttempt.username === userInfo.username &&
+      loginAttempt.password === userInfo.password
+    ) {
+      return "Login successful";
+    } else {
+      return "Invalid credentials";
+    }
+  };
+}
 module.exports = {
   ...(typeof createLoginTracker !== 'undefined' && { createLoginTracker })
 };
